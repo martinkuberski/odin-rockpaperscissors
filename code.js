0539f023dda45d36fcdc6ad426e8d26ad2278d3a
mpk02 <martinkuberski@gmail.com>
2022-07-02 23:26:45 +0100

    Add computerPlay() function

--- code.js
+++ code.js
@@ -1,1 +1,17 @@
-console.log("Hello world!")
+function computerPlay() {
+    switch(Math.floor(Math.random()*3)) {
+        case 0:
+            return "Rock";
+            break;
+        case 1:
+            return "Paper";
+            break;
+        case 2:
+            return "Scissors";
+            break;
+        default:
+            return "ERROR";
+            break;
+    }
+}
+
